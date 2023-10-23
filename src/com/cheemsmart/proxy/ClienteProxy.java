78ba6b361fd0f5df6b50886e07dfdffe72fa3836
MarielCat <marromero@ciencias.unam.mx>
2023-10-23 03:04:20 -0600

    Menu Bug Fixed

--- src/com/cheemsmart/proxy/ClienteProxy.java
+++ src/com/cheemsmart/proxy/ClienteProxy.java
@@ -1,27 +1,46 @@
 package com.cheemsmart.proxy;
 
+/**
+ * Esta clase representa un proxy de cliente que actúa como intermediario para realizar pagos en nombre del cliente real.
+ */
 public class ClienteProxy implements ICliente {
 
-	private ICliente cliente;
-	private int cuentaBancaria;
+    private ICliente cliente;
+    private int cuentaBancaria;
 
-	public ClienteProxy(ICliente cliente) {
-		this.cliente = cliente;
-		cuentaBancaria = cliente.getCuenta();
-	}
-	
-	@Override
-	public void pagar(int cuentaBancaria, double precio) {
-		if(this.cuentaBancaria == cuentaBancaria) {
-			cliente.pagar(cuentaBancaria, precio);
-		} else {
-			throw new IllegalArgumentException();
-		}
-	}
+    /**
+     * Constructor de la clase ClienteProxy.
+     *
+     * @param cliente Cliente real al que se harán las solicitudes de pago.
+     */
+    public ClienteProxy(ICliente cliente) {
+        this.cliente = cliente;
+        cuentaBancaria = cliente.getCuenta();
+    }
 
-	@Override
-	public int getCuenta() {
-		return cuentaBancaria;
-	}
+    /**
+     * Realiza un pago en nombre del cliente real si la cuenta bancaria coincide.
+     *
+     * @param cuentaBancaria Número de cuenta bancaria proporcionado para el pago.
+     * @param precio         Precio del artículo o servicio que se va a pagar.
+     * @throws IllegalArgumentException Si la cuenta bancaria no coincide con la del cliente real.
+     */
+    @Override
+    public void pagar(int cuentaBancaria, double precio) {
+        if (this.cuentaBancaria == cuentaBancaria) {
+            cliente.pagar(cuentaBancaria, precio);
+        } else {
+            throw new IllegalArgumentException("La cuenta bancaria no coincide con la del cliente real.");
+        }
+    }
 
+    /**
+     * Obtiene el número de cuenta bancaria del cliente real.
+     *
+     * @return Número de cuenta bancaria del cliente real.
+     */
+    @Override
+    public int getCuenta() {
+        return cuentaBancaria;
+    }
 }
--- src/com/cheemsmart/proxy/ICliente.java
+++ src/com/cheemsmart/proxy/ICliente.java
@@ -1,8 +1,21 @@
 package com.cheemsmart.proxy;
-
+/**
+ * Esta interfaz representa un cliente genérico que puede realizar pagos.
+ */
 public interface ICliente {
 
-	void pagar(int cuenta, double precio);
+    /**
+     * Realiza un pago utilizando la cuenta bancaria especificada y el precio indicado.
+     *
+     * @param cuenta Cuenta bancaria desde la cual se realizará el pago.
+     * @param precio Precio del artículo o servicio que se va a pagar.
+     */
+    void pagar(int cuenta, double precio);
 
-	int getCuenta();
+    /**
+     * Obtiene el número de cuenta bancaria del cliente.
+     *
+     * @return Número de cuenta bancaria del cliente.
+     */
+    int getCuenta();
 }
